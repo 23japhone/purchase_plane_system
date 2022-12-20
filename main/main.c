dab6cf55a1898b4af37ca1d56edbc782480c5ee3
23japhone <114569289+23japhone@users.noreply.github.com>
2022-12-20 18:04:30 +0800

    Add files via upload

---
+++ main/main.c
@@ -0,0 +1,22 @@
+/*
+ * @Author: 23japhone
+ * @Date: 2022-11-27 15:21:52
+ * @LastEditors: 23japhone
+ * @LastEditTime: 2022-12-20 02:17:27
+ * @description: Ö÷º¯Êý
+ */
+#include <stdio.h>
+#include "admin.h"
+#include "gui.h"
+#include "TICKETS.h"
+#include "user.h"
+#include "function.h"
+
+
+int main()
+{
+    menuRes();
+    return 0;
+}
+
+
